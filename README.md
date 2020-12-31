6d297e922d9173c2bc8fd28f83fccf3a67d7590e
Mingsicogi <codedoctor119@gmail.com>
2021-01-01 05:21:10 +0900

    demo game

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Make a game as Python
 
+# Installation
 1. $pip3 install pygame
+
+
+# Avoiding DDONG
+
+![image](./demo1_screen1.png)
+
+![image](./demo2_screen2.png)
--- pygame_basic/9_quiz_demo_1.py
+++ pygame_basic/9_quiz_demo_1.py
@@ -44,56 +44,106 @@ ddong_y_pos = 0
 character_speed = 2.5
 ddong_speed = 5
 
+# font
+score_font = pygame.font.Font(None, 80)
+game_over_font = pygame.font.Font(None, 100)
+
+# text data
+score = 0
+reward_basic_score = 100
+game_over_text = "GAME OVER"
+game_start = True
 running = True
-while running:
-    dt = clock.tick(frames)
 
-    # 이벤트 처리(키보드)
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT:
-            running = False
-        
-        # 게임 캐릭터 위치 정의
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                character_x_pos -= character_speed
-            if event.key == pygame.K_RIGHT:
-                character_x_pos += character_speed
-            else:
-                pass
-
-        # 게임 캐릭터의 경계 처리
-        if character_x_pos < 0:
-            character_x_pos = 0
-        elif character_x_pos > screen_width - character_width:
-            character_x_pos = screen_width - character_width
-
-    # 매 루프마다 똥을 이동시킴
-    ddong_y_pos += ddong_speed
-    if ddong_y_pos > screen_height:
-        ddong_x_pos = random.randrange(0, screen_width - ddong_width)
-        ddong_y_pos = 0
-
-    # 충돌 처리
-    character_rect = character.get_rect()
-    character_rect.left = character_x_pos
-    character_rect.top = character_y_pos
-
-    ddong_rect = ddong.get_rect()
-    ddong_rect.left = ddong_x_pos
-    ddong_rect.top = ddong_y_pos
-
-    if character_rect.colliderect(ddong_rect):
-        print("Game Over!!!")
-        running = False
-
-    # 화면에 그리기
+# 화면에 그리기
+def game_start_draw_screen(character_x_pos, character_y_pos ,ddong_x_pos, ddong_y_pos):
     screen.fill((0, 0, 255)) # 흰색 배경
     screen.blit(character, (character_x_pos, character_y_pos))
     screen.blit(ddong, (ddong_x_pos, ddong_y_pos))
+    screen.blit(score_font.render(str(score), True, (0,0,0)), (screen_width - 100, 20))
 
-    pygame.display.update() # 화면 업데이트
+while game_start:
+    while running:
+        dt = clock.tick(frames)
+
+        # 이벤트 처리(키보드)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                game_start = False
+            
+            # 게임 캐릭터 위치 정의
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    character_x_pos -= character_speed
+                if event.key == pygame.K_RIGHT:
+                    character_x_pos += character_speed
+                else:
+                    pass
+
+            # 게임 캐릭터의 경계 처리
+            if character_x_pos < 0:
+                character_x_pos = 0
+            elif character_x_pos > screen_width - character_width:
+                character_x_pos = screen_width - character_width
+
+        # 매 루프마다 똥을 이동시킴
+        ddong_y_pos += ddong_speed
+        if ddong_y_pos > screen_height: # 똥피하기 성공(똥 위치 초기화, 점수 획득)
+            ddong_x_pos = random.randrange(0, screen_width - ddong_width)
+            ddong_y_pos = 0
+            score += reward_basic_score
+
+        # 충돌 처리
+        character_rect = character.get_rect()
+        character_rect.left = character_x_pos
+        character_rect.top = character_y_pos
+
+        ddong_rect = ddong.get_rect()
+        ddong_rect.left = ddong_x_pos
+        ddong_rect.top = ddong_y_pos
+
+        if character_rect.colliderect(ddong_rect): # game over
+            screen.fill((0, 0, 0))
+            screen.blit(game_over_font.render(game_over_text, True, (255, 0, 0)), (screen_width * 0.05, screen_height * 0.4))
+            score = 0
+            running = False
+        else:
+            game_start_draw_screen(character_x_pos, character_y_pos, ddong_x_pos, ddong_y_pos)
+
+        pygame.display.update() # 화면 업데이트
+    
+    
+    event = pygame.event.wait(1000)
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_SPACE:
+            character_x_pos = (screen_width / 2) - (character_width / 2)
+            character_y_pos = screen_height - character_height
+            ddong_x_pos = random.randrange(0, screen_width - ddong_width)
+            ddong_y_pos = 0
+            
+            character_rect = character.get_rect()
+            character_rect.left = character_x_pos
+            character_rect.top = character_y_pos
+
+            ddong_rect = ddong.get_rect()
+            ddong_rect.left = ddong_x_pos
+            ddong_rect.top = ddong_y_pos
+
+            game_start_draw_screen((screen_width / 2) - (character_width / 2), screen_height - character_height, ddong_rect.left, ddong_rect.top)
+            running = True
+            game_start = True
+            
+        elif event.key == pygame.K_ESCAPE:
+            game_start = False
+            running = False
+
+    elif event.type == pygame.QUIT:
+        running = False
+        game_start = False
 
 # pygame 종료
 pygame.quit()
+
+
+
