2bbe9db033f15f156a0a36e53f20bd86cac0a186
Mingsicogi <codedoctor119@gmail.com>
2021-01-01 00:26:25 +0900

    background

--- pygame_basic/2_background.py
+++ pygame_basic/2_background.py
@@ -1,25 +1,33 @@
 import pygame
 
-pygame.init()
+pygame.init() # 초기화(반드시 필요)
 
-width = 480
-height = 640
+# 화면 크기 설정
+screen_width = 480 # 가로
+screen_height = 640 # 세로
 
-screen = pygame.display.set_mode(((width, height)))
+screen = pygame.display.set_mode((screen_width, screen_height))
 
-pygame.display.set_caption("Mins game")
+# 화면 타이틀 설정
+pygame.display.set_caption("Mins Game")
 
-# 이미지 가져오기
+# 배경화면 이미지 가져오기
 background = pygame.image.load("/Users/minseok/pythonProject/pygame_basic/background.jpg")
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            print("게임을 종료합니다.")
 
-    screen.blit(background, (0, 0)) # 배경 그리기
-    # screen.fill((0,0,255)) # r g b 로 채우기
+# 이벤트 루프
+running = True # 게임이 진행중인지 여부
+while running:
+    for event in pygame.event.get(): # 사용자의 이벤트를 계속 얻어옴
+        if event.type == pygame.QUIT:
+            print("게임을 종료합니다.")
+            running = False
+
+        screen.blit(background, (0, 0)) # 배경 그리기
+        # screen.fill((0,0,255)) # r g b 로 채우기
 
     pygame.display.update() # 화면 업데이트
+
+
+# pygame 종료
+pygame.quit()
